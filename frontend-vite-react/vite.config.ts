e320d788b1f94df81caded54ff215bd928746aa9
thomasjakkel <thomas.jakkel@gmail.com>
2024-06-30 17:48:07 +0200

    frontend: Added one folder setup for each component

--- frontend-vite-react/vite.config.ts
+++ frontend-vite-react/vite.config.ts
@@ -3,6 +3,7 @@
 
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { configDefaults } from 'vitest/config'
 import * as path from 'path'
 
 // https://vitejs.dev/config/
@@ -11,14 +12,19 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: ['src/setupTests.ts'],
+    setupFiles: ['./src/setupTests.ts'],
     coverage: {
       // https://vitest.dev/config/#coverage
       provider: 'v8',
       reporter: ['text', 'html', 'json'],
+      include: ['src/**/*'],
+      exclude: [...configDefaults.exclude],
+    },
+    alias: {
+      '@': path.resolve(__dirname, './src'),
     },
   },
   resolve: {
-    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
+    alias: [{ find: '@', replacement: path.resolve(__dirname, './src') }],
   },
 })
